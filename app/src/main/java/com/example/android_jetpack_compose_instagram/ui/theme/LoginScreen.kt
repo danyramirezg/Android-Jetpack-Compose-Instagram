43fbbd090531bb5acabfc79f05ac9a9549070bbc
danyramirezg <danyramirezg@gmail.com>
2023-05-17 16:55:02 -0500

    Add UI details to the Login Screen

--- app/src/main/java/com/example/android_jetpack_compose_instagram/ui/theme/LoginScreen.kt
+++ app/src/main/java/com/example/android_jetpack_compose_instagram/ui/theme/LoginScreen.kt
@@ -1,9 +1,7 @@
 package com.example.android_jetpack_compose_instagram.ui.theme
 
 import android.app.Activity
-import android.graphics.Paint.Align
-import androidx.annotation.StringRes
-import androidx.compose.animation.AnimatedContentScope.SlideDirection.Companion.Start
+import android.util.Patterns
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -16,14 +14,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,23 +39,24 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.text.buildSpannedString
 import com.example.android_jetpack_compose_instagram.R
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
-import java.io.StringReader
 
 @Composable
 fun LoginScreen() {
     Box(
         Modifier
             .fillMaxSize()
-            .padding(8.dp)) {
+            .padding(8.dp)
+    ) {
         InstagramHeader(Modifier.align(Alignment.TopEnd))
         InstagramBody(Modifier.align(Alignment.Center))
+        footer(Modifier.align(Alignment.BottomCenter))
     }
 }
 
@@ -59,14 +64,20 @@ fun LoginScreen() {
 fun InstagramBody(modifier: Modifier) {
     var email by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
-    var isLoginEnable by rememberSaveable { mutableStateOf(true)}
+    var isLoginEnable by rememberSaveable { mutableStateOf(true) }
 
     Column(modifier = modifier) {
         InstagramLogo(modifier = Modifier.align(Alignment.CenterHorizontally))
         Spacer(modifier = Modifier.size(16.dp))
-        InstagramEmail(email) { email = it }
+        InstagramEmail(email) {
+            email = it
+            isLoginEnable = enableLogin(email, password)
+        }
         Spacer(modifier = Modifier.size(4.dp))
-        InstagramPassword(password) { password = it }
+        InstagramPassword(password) {
+            password = it
+            isLoginEnable = enableLogin(email, password)
+        }
         Spacer(modifier = Modifier.size(8.dp))
         ForgotPasswordText(modifier = Modifier.align(Alignment.End))
         Spacer(modifier = Modifier.size(16.dp))
@@ -79,21 +90,63 @@ fun InstagramBody(modifier: Modifier) {
 }
 
 @Composable
-fun SocialLogin(modifier: Modifier) {
-    Row(modifier = modifier
-        .fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
-        Image(painterResource(id = R.drawable.fb), contentDescription = "Social Media Logo", Modifier.size(16.dp))
-        Text(text = stringResource(id = R.string.social_media_login), color = Color(0xFF4EA8E9), fontSize = 14.sp, fontWeight = FontWeight.Bold,
-        modifier = Modifier.padding(horizontal = 8.dp))
-        }
+fun footer(modifier: Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(24.dp)
+    ) {
+        DividerCustomized(modifier = modifier.fillMaxWidth())
+        Spacer(modifier = Modifier.size(16.dp))
+        SignUp()
     }
+}
+
+@Composable
+fun SignUp() {
+    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+        Text(
+            text = stringResource(id = R.string.not_have_account),
+            fontSize = 12.sp,
+            color = Color(0xFFB5B5B5)
+        )
+        Text(
+            text = stringResource(id = R.string.sign_up),
+            Modifier.padding(horizontal = 6.dp),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF4EA8E9)
+        )
+    }
+}
+
+@Composable
+fun SocialLogin(modifier: Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painterResource(id = R.drawable.fb),
+            contentDescription = "Social Media Logo",
+            Modifier.size(16.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.social_media_login),
+            color = Color(0xFF4EA8E9),
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(horizontal = 8.dp)
+        )
+    }
+}
 
 @Composable
 fun LoginDivider() {
     Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-        Divider(modifier = Modifier
-            .height(1.dp)
-            .weight(1f), color = Color.LightGray)
+        DividerCustomized(Modifier.weight(1f))
         Text(
             "OR",
             modifier = Modifier.padding(horizontal = 18.dp),
@@ -101,45 +154,115 @@ fun LoginDivider() {
             fontWeight = FontWeight.Bold,
             color = Color(0xFFB5B5B5)
         )
-        Divider(modifier = Modifier
-            .height(1.dp)
-            .weight(1f), color = Color.LightGray)
+        DividerCustomized(Modifier.weight(1f))
     }
+}
+
+@Composable
+fun DividerCustomized(modifier: Modifier) {
+    Divider(
+        modifier = modifier.height(1.dp), color = Color.LightGray
+    )
 }
 
 @Composable
 fun LoginButton(loginEnable: Boolean) {
-    Button(onClick = { }, enabled = loginEnable, modifier = Modifier.fillMaxWidth()) {
+    Button(
+        onClick = { }, enabled = loginEnable, modifier = Modifier.fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(
+            //backgroundColor = Color(0xFF4EA8E9),
+            //disableBackgroundColor = Color(0xFF78C8F9),
+            contentColor = Color.White,
+            disabledContentColor = Color.White
+        )
+    ) {
         Text(text = stringResource(id = R.string.login_text), fontWeight = FontWeight.Bold)
     }
 }
 
+fun enableLogin(email: String, password: String) =
+    Patterns.EMAIL_ADDRESS.matcher(email).matches() && password.length > 6
+
 @Composable
 fun ForgotPasswordText(modifier: Modifier) {
-        Text(text = stringResource(id = R.string.forgot_password), modifier = modifier
+    Text(
+        text = stringResource(id = R.string.forgot_password), modifier = modifier
             .clickable { },
-            color = Color(0xFF4EA8E9), fontSize = 12.sp, fontWeight = FontWeight.Bold)
+        color = Color(0xFF4EA8E9), fontSize = 12.sp, fontWeight = FontWeight.Bold
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InstagramPassword(password: String, onTextChanged: (String) -> Unit) {
-    TextField(value = password, onValueChange = {onTextChanged(it)}, modifier = Modifier.fillMaxWidth() )
+    var passwordVisibility by remember { mutableStateOf(false) }
+    TextField(
+        value = password, onValueChange = { onTextChanged(it) },
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text = "Password") },
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color(0xFFB2B2B2),
+            //backgroundColor = Color(0xfffAFAFA),
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        singleLine = true,
+        maxLines = 1,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+
+
+        trailingIcon = {
+            val image = if (passwordVisibility) {
+                Icons.Filled.KeyboardArrowLeft
+            } else {
+                Icons.Filled.Lock
+            }
+            IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
+                Icon(imageVector = image, contentDescription = "Icon password")
+            }
+        },
+        visualTransformation = if (passwordVisibility) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        }
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InstagramEmail(email: String, onTextChanged: (String) -> Unit) {
-    TextField(value = email, onValueChange = { onTextChanged(it) }, modifier = Modifier.fillMaxWidth())
+    TextField(
+        value = email,
+        onValueChange = { onTextChanged(it) },
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text = "Email") },
+        singleLine = true,
+        maxLines = 1,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color(0xFFB2B2B2),
+            // backgroundColor = Color(0xfffAFAFA),
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        )
+    )
 }
 
 @Composable
 fun InstagramLogo(modifier: Modifier) {
-    Image(painter = painterResource(id = R.drawable.insta), contentDescription = "logo", modifier = modifier)
+    Image(
+        painter = painterResource(id = R.drawable.insta),
+        contentDescription = "logo",
+        modifier = modifier
+    )
 }
 
 @Composable
 fun InstagramHeader(modifier: Modifier) {
     val activity = LocalContext.current as Activity
-    Icon(imageVector = Icons.Default.Close, contentDescription = "Close App", modifier.clickable { activity.finish() })
+    Icon(
+        imageVector = Icons.Default.Close,
+        contentDescription = "Close App",
+        modifier.clickable { activity.finish() })
 }
